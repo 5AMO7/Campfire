41814ce853d1562b9c9a48b2fa8cbd8f02c2d3db
Kaskapa <100031511+Kaskapa@users.noreply.github.com>
2023-07-02 00:52:16 +0300

    Login.js
    The error "User not found" was not implemented so I implemented it.
    I added a feature where you can press enter instead of pressing the "Enter the wild!" button for faster and more effective way to log in, I also plan on implementing the same  feature for register, but for now login will be just fine.
    TODO: I think there is nothing to do for the login page for now, only thing that comes up in my mind is the ability to login with your Google account, otherwise the login.js is done. If there are any problems they will pop up in the notion to do list.

--- public/login/login.js
+++ public/login/login.js
@@ -7,34 +7,43 @@ export const loginauth = auth;
 const loginButton = document.getElementById('loginBtn');
 
 document.getElementById('emailField').oninput = function() {
-document.getElementById("emailError").innerText = "";
+    document.getElementById("emailError").innerText = "";
 }
 
 document.getElementById('passField').oninput = function() {
-document.getElementById("passwordError").innerText = "";
+    document.getElementById("passwordError").innerText = "";
 }
 
 loginButton.addEventListener('click',(e) => {
+    login();
+});
 
+document.querySelectorAll('input').forEach( el => {
+    el.addEventListener('keydown', e => {
+        if(e.code === "Enter") {
+           login();
+        }
+    })
+})
+
+function login(){
     var email = document.getElementById('emailField').value;
     var password = document.getElementById('passField').value;
 
     signInWithEmailAndPassword(auth, email, password)
-
     .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
+        // Signed in 
+        const user = userCredential.user;
 
-    location.href = '../main/index.html';
-    // ...
+        location.href = '../main/index.html';
+        // ...
     })
-
     .catch((error) => {
         var errorCode = error.code;
 
         errorCode = errorCode.substring(5);
         errorCode = errorCode.replaceAll("-", " ");
-        console.log(errorCode);
+        errorCode = errorCode.trim();
 
         switch(errorCode){
             case "invalid email": errorCode = "Please provide a valid E-mail";
@@ -42,13 +51,16 @@ loginButton.addEventListener('click',(e) => {
             case "missing password": errorCode = "Please provide a Password";
             break;
             case "wrong password": errorCode = "Wrong Password";
+            break;
+            case "user not found": errorCode = "User not found";
+            break;
         }
             
-        if(errorCode.includes("E-mail")){
+        if(errorCode.includes("E-mail") || errorCode.includes("User")){
             document.getElementById('emailError').innerText = errorCode;
         }
         if(errorCode.includes("Password")){
             document.getElementById('passwordError').innerText = errorCode;
         }
     });
-});
+}
